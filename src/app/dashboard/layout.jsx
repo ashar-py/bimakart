21a276fa0d0aeff8ec029895198505478b418de8
rash <ashar.shakil04@gmail.com>
2024-02-29 15:07:35 +0530

    Stats Page changes

--- src/app/dashboard/layout.jsx
+++ src/app/dashboard/layout.jsx
@@ -24,16 +24,13 @@
 "use client";
 
 import Sidebar from "@/components/sidebar/sidebar";
-
 import styles from "@/ui/dashboard/dashboard.module.css";
 import Footer from "@/components/footer/footer";
 
 const Layout = ({ children }) => {
   return (
     <div className={styles.container}>
-      <div >
-        <Sidebar />
-      </div>
+      <Sidebar />
       <div className={styles.content}>
         {children}
         <Footer />
